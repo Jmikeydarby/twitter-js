f6277ec304654e060699581bdf3328a9cf0c233f
Joey Darbyshire <jmikeydarby1@gmail.com>
2016-11-08 11:19:30 -0500

    Rendered a template, added lodash, and created tweetbank.js

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const chalk = require('chalk');
 const morgan = require('morgan');
+const nunjucks = require('nunjucks');
 const app = express();
 
 
 app.listen(3000);
 
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+nunjucks.configure('views', {noCache: true});
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
 
 app.use(morgan(chalk.blue(':method') + chalk.red(' :url') + chalk.green(' :status')));
-
-// app.use(morgan(chalk.blue(':method')));
-// app.use(morgan(chalk.red(':url')));
-// app.use(morgan(chalk.green(':status')));
 
 app.use("/special", function(req, res, next){
 	console.log("You have entered the danger zone!!!");
@@ -19,9 +30,8 @@ app.use("/special", function(req, res, next){
 
 });
 
-app.get("/", function(req, res){
-
-	res.send("Hello World");
+app.get("/views", function(req, res){
+	res.render('index', locals );
 });
 
 app.get("/news", function(req, res){
