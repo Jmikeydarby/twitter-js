3415a2c08198593f1d15bc83612ccf49e6433df3
Joey Darbyshire <jmikeydarby1@gmail.com>
2016-11-08 12:40:16 -0500

    final edits

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,27 @@
+'use strict'
+
 const express = require('express');
 const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
 
+router.use(express.static('public'));
+
+router.get('/stylesheets/', function (req, res) {
+	// console.log(__dirname);
+  	res.sendFile('/stylesheets/');
+});
+
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
   res.render( 'index', { tweets: tweets } );
 });
+
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  res.render( 'index', { tweets : list } );
+});
+
 
 module.exports = router;
