62e9536f0a898d6dbeef9df311317b7b0d548218
Joey Darbyshire <jmikeydarby1@gmail.com>
2016-11-08 11:39:35 -0500

    updated tweetbank.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const chalk = require('chalk');
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
+const routes = require('./routes/');
 const app = express();
 
 
@@ -24,17 +25,6 @@ app.engine('html', nunjucks.render);
 
 app.use(morgan(chalk.blue(':method') + chalk.red(' :url') + chalk.green(' :status')));
 
-app.use("/special", function(req, res, next){
-	console.log("You have entered the danger zone!!!");
-	next();
+app.use('/', routes);
 
-});
 
-app.get("/views", function(req, res){
-	res.render('index', locals );
-});
-
-app.get("/news", function(req, res){
-
-	res.send("Hello News");
-});
--- tweetBank.js
+++ tweetBank.js
@@ -1,1 +1,22 @@
+'use strict'
 const _ = require('lodash');
+
+var data = [];
+
+function add (name, content) {
+  data.push({ name: name, content: content });
+}
+
+function list () {
+  return _.cloneDeep(data);
+}
+
+function find (properties) {
+  return _.cloneDeep(_.filter(data, properties));
+}
+
+module.exports = { 
+	add: add, 
+	list: list, 
+	find: find 
+}
